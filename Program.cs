e3586c1bff3fd8cfd360ada9165af37df38c6799
XHA-dev <96320545+XHA-dev@users.noreply.github.com>
2021-12-26 19:16:38 +0000

    Combine 2 Numbers

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Giraffe
+{
+    class Program {
+
+        static void Main(string[] args) {
+
+            int a = 241;
+            int b = 333;
+            Console.WriteLine(a + b);
+
+        }
+    }       
+}
